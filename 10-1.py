953b80865d2c2e315f7933592933284244da7ad0
lyjason <lya1b2333@hotmail.com>
2016-07-10 20:10:02 +0800

    commit 10-1

---
+++ 10-1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+#-*-coding:utf-8-*-
+print 'imooc test 10-1 生成列表'
+
+print '请利用列表生成式生成列表[1x2,3x4,5x6,7x8,......,99x100]'
+#提示 range（1,100,2）可以生成list[1,3,5,7,9.....]
+print [x * (x+1) for x in range(1,100,2) ]
