0ee6ab5b69ff8aa7cab0106adb6baf0f059cbeea
lyjason <lya1b2333@hotmail.com>
2016-07-10 19:45:46 +0800

    add 9-4.py

---
+++ 9-4.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+#-*-coding:utf-8-*-
+#mooc test 9-4 迭代dict的key和valuev
+d = {'adam':95,'lisa':85,'bart':59,'paul':74}
+l = d.items()
+sum = 0.0
+for k ,v in l:
+	sum = sum + v 
+	print k,':',v
+print 'average',':',sum/len(l)
