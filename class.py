a2fce3c10f5ce99d7b3ee806b21a1f5e8e8503ea
root <root@jasonOS.(none)>
2016-08-18 22:00:30 +0800

    v2.0

--- class.py
+++ class.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python
 #-*-coding:utf-8-*-
 #51CTO课程学习练习
+import random
+answer = random.randint(0,10)
 print('****************jason****************')
-
-num=input('随便输入一个数字来验证对不对：')
+num=input('随便输入一个数字来试试运气：')
 num=int(num)
-if num == 8 :
-	print('你猜对了，好厉害')
-else:
-	print('猜错啦，哈哈哈')
+i=3;
+while i!=0  :
+	if num == answer :
+		print('你猜对了，好厉害')
+		break
+	else:
+		if num>answer :
+			print('不好，大了。')
+		else:
+			print('哎，小了。')
+	i=i-1
+	if i!=0 :	
+		num=input('请重新输入一个值：')
+		num=int(num)
+	else :
+		print('尝试次数已耗尽')
 print('到此结束')
