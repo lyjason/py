74d9b29a590c83112152cd2e251e9d461d3ddb15
root <root@jasonOS.(none)>
2016-08-18 20:58:27 +0800

    class

---
+++ class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#-*-coding:utf-8-*-
+#51CTO课程学习练习
+print('****************jason****************')
+
+num=input('随便输入一个数字来验证对不对：')
+num=int(num)
+if num == 8 :
+	print('你猜对了，好厉害')
+else:
+	print('猜错啦，哈哈哈')
+print('到此结束')
